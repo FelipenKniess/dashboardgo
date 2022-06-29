9299695ebe4c35857e51b99690dff6007b515a83
FelipeNKniess <felipe.nkniess@gmail.com>
2022-06-29 01:12:41 -0300

    created the initial structure of project

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  styles: {
+    colors: {
+      gray: {
+        "900": "#FFF",
+      },
+    },
+    global: {
+      body: {
+        bg: "gray.900",
+        color: "gray.50",
+      },
+    },
+  },
+});
